cd04d4a6fa80c8c9bb90f79b0db0123d69e09cac
Caleb Amsden <caleb.amsden@gmail.com>
2022-08-02 13:58:36 -0700

    add noreferrer attribute to link

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,11 @@ export function MyHeader() {
   return (
     <div className={styles.container}>
       <div className={styles.title}>Welcome to the Builder Demo Editor</div>
-      <a href="https://github.com/Builderio/demo-editor" target="_blank">
+      <a
+        href="https://github.com/Builderio/demo-editor"
+        target="_blank"
+        rel="noreferrer"
+      >
         View the sample code for this Next.js project
       </a>
     </div>
