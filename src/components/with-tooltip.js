a3ded1c6d7d232aceb2a455f14ed04d2ac3a607b
Steve Sewell <steve@builder.io>
2022-09-02 16:26:47 -0700

    Fix lint

--- src/components/with-tooltip.js
+++ src/components/with-tooltip.js
@@ -2,28 +2,30 @@ import Tooltip from "@mui/material/Tooltip";
 
 // HOC to add a tooltip to a component's source on hover, for demo purposes
 export function withTooltip(url, Component) {
-  return (props) => (
-    <Tooltip
-      title={
-        <div
-          style={{
-            fontSize: 14,
-            fontWeight: "bold",
-            cursor: 'pointer'
-          }}
-          onClick={() => {
-            // Open with JS so will open in visual editor 
-            // (by default links are intentionally disabled in the visual editor)
-            open(url, "_blank");
-          }}
-        >
-          Click here to view my source code
+  return function SourceCodeTooltipWrappedComponent(props) {
+    return (
+      <Tooltip
+        title={
+          <div
+            style={{
+              fontSize: 14,
+              fontWeight: "bold",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              // Open with JS so will open in visual editor
+              // (by default links are intentionally disabled in the visual editor)
+              open(url, "_blank");
+            }}
+          >
+            Click here to view my source code
+          </div>
+        }
+      >
+        <div>
+          <Component {...props} />
         </div>
-      }
-    >
-      <div>
-        <Component {...props} />
-      </div>
-    </Tooltip>
-  );
+      </Tooltip>
+    );
+  };
 }
